066e1ec5ba8b76e06bc58d37e81b8d4963971b7c
Forcica <bforcica@gmail.com>
2024-02-23 03:26:45 +0100

    [FIX] Non important changes

--- alpha_applying_forci.py
+++ alpha_applying_forci.py
@@ -1,87 +1,87 @@
 bl_info = {
-    "name": "Forci Alpha Changing",
-    "author": "Votre nom",
-    "version": (1, 0),
-    "blender": (2, 80, 0),
-    "location": "View3D > Tool",
-    "description": "Change alpha settings for Principled BSDF Materials",
-    "category": "FORCI STUFF",
+   "name": "Forci Alpha Changing",
+   "author": "Votre nom",
+   "version": (1, 0),
+   "blender": (2, 80, 0),
+   "location": "View3D > Tool",
+   "description": "Change alpha settings for Principled BSDF Materials",
+   "category": "FORCI STUFF",
 }
 
 import bpy
 
 class ForciAlphaApplyingOperator(bpy.types.Operator):
-    """Apply Alpha Texture to Principled BSDF Material"""
-    bl_idname = "forcica.alpha_applying"
-    bl_label = "Apply Alpha Texture"
-    
-    def execute(self, context):
-        obj = context.active_object
-        if obj is not None and obj.type == 'MESH':
-            for material in obj.data.materials:
-                if material.use_nodes:
-                    bsdf_node = None
-                    image_node = None
-                    # Trouver le nœud BSDF Principled et le nœud d'image connecté
-                    for node in material.node_tree.nodes:
-                        if node.type == 'BSDF_PRINCIPLED':
-                            bsdf_node = node
-                        elif node.type == 'TEX_IMAGE':
-                            # Vérifier si ce nœud d'image est connecté au Base Color de BSDF Principled
-                            for link in material.node_tree.links:
-                                if link.from_node == node and link.to_node.type == 'BSDF_PRINCIPLED' and link.to_socket.name == 'Base Color':
-                                    image_node = node
-                                    break
-                    if bsdf_node and image_node:
-                        # Connecter le canal Alpha de la texture image au canal Alpha de Principled BSDF
-                        alpha_output = image_node.outputs.get('Alpha')
-                        alpha_input = bsdf_node.inputs.get('Alpha')
-                        if alpha_output and alpha_input:
-                            material.node_tree.links.new(alpha_output, alpha_input)
-        self.report({'INFO'}, "Alpha texture applied to Principled BSDF materials")
-        return {'FINISHED'}
+   """Apply Alpha Texture to Principled BSDF Material"""
+   bl_idname = "forcica.alpha_applying"
+   bl_label = "Apply Alpha Texture"
+   
+   def execute(self, context):
+      obj = context.active_object
+      if obj is not None and obj.type == 'MESH':
+         for material in obj.data.materials:
+               if material.use_nodes:
+                  bsdf_node = None
+                  image_node = None
+                  # Trouver le nœud BSDF Principled et le nœud d'image connecté
+                  for node in material.node_tree.nodes:
+                     if node.type == 'BSDF_PRINCIPLED':
+                           bsdf_node = node
+                     elif node.type == 'TEX_IMAGE':
+                           # Vérifier si ce nœud d'image est connecté au Base Color de BSDF Principled
+                           for link in material.node_tree.links:
+                              if link.from_node == node and link.to_node.type == 'BSDF_PRINCIPLED' and link.to_socket.name == 'Base Color':
+                                 image_node = node
+                                 break
+                  if bsdf_node and image_node:
+                     # Connecter le canal Alpha de la texture image au canal Alpha de Principled BSDF
+                     alpha_output = image_node.outputs.get('Alpha')
+                     alpha_input = bsdf_node.inputs.get('Alpha')
+                     if alpha_output and alpha_input:
+                           material.node_tree.links.new(alpha_output, alpha_input)
+      self.report({'INFO'}, "Alpha texture applied to Principled BSDF materials")
+      return {'FINISHED'}
 
 class ForciAlphaBlendOperator(bpy.types.Operator):
-    """Toggle Alpha Blend Mode for Material"""
-    bl_idname = "forcica.alpha_blend"
-    bl_label = "Toggle Alpha Blend Mode"
-    
-    def execute(self, context):
-        # Récupérer l'objet sélectionné
-        obj = context.active_object
-        if obj is not None and obj.type == 'MESH':
-            # Parcourir les matériaux de l'objet
-            for material in obj.data.materials:
-                if material.use_nodes:
-                    for node in material.node_tree.nodes:
-                        if node.type == 'BSDF_PRINCIPLED':
-                            # Changer le mode de fusion en Alpha Blend
-                            material.blend_method = 'BLEND'
-        self.report({'INFO'}, "Alpha Blend mode set for selected materials")
-        return {'FINISHED'}
+   """Toggle Alpha Blend Mode for Material"""
+   bl_idname = "forcica.alpha_blend"
+   bl_label = "Toggle Alpha Blend Mode"
+   
+   def execute(self, context):
+      # Récupérer l'objet sélectionné
+      obj = context.active_object
+      if obj is not None and obj.type == 'MESH':
+         # Parcourir les matériaux de l'objet
+         for material in obj.data.materials:
+               if material.use_nodes:
+                  for node in material.node_tree.nodes:
+                     if node.type == 'BSDF_PRINCIPLED':
+                           # Changer le mode de fusion en Alpha Blend
+                           material.blend_method = 'BLEND'
+      self.report({'INFO'}, "Alpha Blend mode set for selected materials")
+      return {'FINISHED'}
 
 class ForciAlphaChangingPanel(bpy.types.Panel):
-    """Creates a Panel in the Object properties window"""
-    bl_label = "Alpha ON"
-    bl_idname = "MATERIAL_PT_forci_alpha_changing"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'FORCI STUFF'
-    
-    def draw(self, context):
-        layout = self.layout
-        layout.operator(ForciAlphaApplyingOperator.bl_idname)
-        layout.operator(ForciAlphaBlendOperator.bl_idname)
+   """Creates a Panel in the Object properties window"""
+   bl_label = "Alpha ON"
+   bl_idname = "MATERIAL_PT_forci_alpha_changing"
+   bl_space_type = 'VIEW_3D'
+   bl_region_type = 'UI'
+   bl_category = 'FORCI STUFF'
+   
+   def draw(self, context):
+      layout = self.layout
+      layout.operator(ForciAlphaApplyingOperator.bl_idname)
+      layout.operator(ForciAlphaBlendOperator.bl_idname)
 
 def register():
-    bpy.utils.register_class(ForciAlphaApplyingOperator)
-    bpy.utils.register_class(ForciAlphaBlendOperator)
-    bpy.utils.register_class(ForciAlphaChangingPanel)
+   bpy.utils.register_class(ForciAlphaApplyingOperator)
+   bpy.utils.register_class(ForciAlphaBlendOperator)
+   bpy.utils.register_class(ForciAlphaChangingPanel)
 
 def unregister():
-    bpy.utils.unregister_class(ForciAlphaApplyingOperator)
-    bpy.utils.unregister_class(ForciAlphaBlendOperator)
-    bpy.utils.unregister_class(ForciAlphaChangingPanel)
+   bpy.utils.unregister_class(ForciAlphaApplyingOperator)
+   bpy.utils.unregister_class(ForciAlphaBlendOperator)
+   bpy.utils.unregister_class(ForciAlphaChangingPanel)
 
 if __name__ == "__main__":
-    register()
+   register()
--- material_changing_forci.py
+++ material_changing_forci.py
@@ -1,96 +1,96 @@
 bl_info = {
-    "name": "Forci Texture Replacer",
-    "author": "Forcica",
-    "version": (1, 0),
-    "blender": (2, 80, 0),
-    "location": "View3D > Tool > Forci Stuff",
-    "description": "Replace .dds Textures with .png in Principled BSDF for all objects in the scene",
-    "category": "FORCI STUFF",
+   "name": "Forci Texture Replacer",
+   "author": "Forcica",
+   "version": (1, 0),
+   "blender": (2, 80, 0),
+   "location": "View3D > Tool > Forci Stuff",
+   "description": "Replace .dds Textures with .png in Principled BSDF for all objects in the scene",
+   "category": "FORCI STUFF",
 }
 
 import bpy
 import os
 
 class ForciTextureReplaceOperator(bpy.types.Operator):
-    bl_idname = "forcica.texture_replace_operator"
-    bl_label = "Replace Textures"
+   bl_idname = "forcica.texture_replace_operator"
+   bl_label = "Replace Textures"
 
-    def execute(self, context):
-        # Get the texture directory from the addon preferences
-        texture_directory = context.preferences.addons[__name__].preferences.forcica_texture_directory
+   def execute(self, context):
+      # Get the texture directory from the addon preferences
+      texture_directory = context.preferences.addons[__name__].preferences.forcica_texture_directory
 
-        # Iterate through all objects in the scene
-        for obj in bpy.context.scene.objects:
-            if obj.type == 'MESH':
-                # Iterate through the materials of the object
-                for material_slot in obj.material_slots:
-                    material = material_slot.material
-                    if material.use_nodes:
-                        # Iterate through the nodes of the material
-                        for node in material.node_tree.nodes:
-                            if node.type == 'BSDF_PRINCIPLED':
-                                # Check if the Base Color input is linked to an image texture node
-                                base_color_input = node.inputs["Base Color"]
-                                if base_color_input.is_linked:
-                                    linked_node = base_color_input.links[0].from_node
-                                    if linked_node.type == 'TEX_IMAGE':
-                                        image = linked_node.image
-                                        if image:
-                                            extensions = ['.dds', '.dds.001', '.dds.002', '.dds.003', '.dds.004', '.dds.005']
-                                            if any(image.filepath.lower().endswith(ext) for ext in extensions):
-                                                # Remove the .dds extensions from the image name
-                                                base_name = os.path.splitext(image.name)[0]
-                                                for ext in extensions:
-                                                    base_name = base_name.replace(ext, '')
-                                                png_filename = base_name + ".png"
+      # Iterate through all objects in the scene
+      for obj in bpy.context.scene.objects:
+         if obj.type == 'MESH':
+               # Iterate through the materials of the object
+               for material_slot in obj.material_slots:
+                  material = material_slot.material
+                  if material.use_nodes:
+                     # Iterate through the nodes of the material
+                     for node in material.node_tree.nodes:
+                           if node.type == 'BSDF_PRINCIPLED':
+                              # Check if the Base Color input is linked to an image texture node
+                              base_color_input = node.inputs["Base Color"]
+                              if base_color_input.is_linked:
+                                 linked_node = base_color_input.links[0].from_node
+                                 if linked_node.type == 'TEX_IMAGE':
+                                       image = linked_node.image
+                                       if image:
+                                          extensions = ['.dds', '.dds.001', '.dds.002', '.dds.003', '.dds.004', '.dds.005']
+                                          if any(image.filepath.lower().endswith(ext) for ext in extensions):
+                                             # Remove the .dds extensions from the image name
+                                             base_name = os.path.splitext(image.name)[0]
+                                             for ext in extensions:
+                                                   base_name = base_name.replace(ext, '')
+                                             png_filename = base_name + ".png"
 
-                                                # Get the full path to the PNG file using os.path.join
-                                                png_filepath = os.path.join(texture_directory, png_filename)
+                                             # Get the full path to the PNG file using os.path.join
+                                             png_filepath = os.path.join(texture_directory, png_filename)
 
-                                                if os.path.isfile(png_filepath):
-                                                    new_img = bpy.data.images.load(png_filepath)
+                                             if os.path.isfile(png_filepath):
+                                                   new_img = bpy.data.images.load(png_filepath)
 
-                                                    # Create a new Image Texture node and add it to Shading
-                                                    new_texture_node = material.node_tree.nodes.new(type="ShaderNodeTexImage")
-                                                    new_texture_node.image = new_img
+                                                   # Create a new Image Texture node and add it to Shading
+                                                   new_texture_node = material.node_tree.nodes.new(type="ShaderNodeTexImage")
+                                                   new_texture_node.image = new_img
 
-                                                    # Link the new Image Texture node to the Base Color input
-                                                    material.node_tree.links.new(new_texture_node.outputs["Color"], node.inputs["Base Color"])
-                                                    self.report({'INFO'}, f"Replaced texture {image.name} with {png_filename}")
-                                                else:
-                                                    self.report({'WARNING'}, f"PNG texture {png_filename} not found in directory")
+                                                   # Link the new Image Texture node to the Base Color input
+                                                   material.node_tree.links.new(new_texture_node.outputs["Color"], node.inputs["Base Color"])
+                                                   self.report({'INFO'}, f"Replaced texture {image.name} with {png_filename}")
+                                             else:
+                                                   self.report({'WARNING'}, f"PNG texture {png_filename} not found in directory")
 
-        return {'FINISHED'}
+      return {'FINISHED'}
 
 class ForciTextureReplacePanel(bpy.types.Panel):
-    bl_label = "Texture Replacer (DDS to PNG)"
-    bl_idname = "OBJECT_PT_forci_texture_replacer"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'FORCI STUFF'
+   bl_label = "Texture Replacer (DDS to PNG)"
+   bl_idname = "OBJECT_PT_forci_texture_replacer"
+   bl_space_type = 'VIEW_3D'
+   bl_region_type = 'UI'
+   bl_category = 'FORCI STUFF'
 
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(context.preferences.addons[__name__].preferences, "forcica_texture_directory", text="Texture Directory")
-        layout.operator(ForciTextureReplaceOperator.bl_idname)
+   def draw(self, context):
+      layout = self.layout
+      layout.prop(context.preferences.addons[__name__].preferences, "forcica_texture_directory", text="Texture Directory")
+      layout.operator(ForciTextureReplaceOperator.bl_idname)
 
 class ForciTexturePreferences(bpy.types.AddonPreferences):
-    bl_idname = __name__
+   bl_idname = __name__
 
-    forcica_texture_directory: bpy.props.StringProperty(
-        name="Texture Directory",
-        subtype='DIR_PATH'
-    )
+   forcica_texture_directory: bpy.props.StringProperty(
+      name="Texture Directory",
+      subtype='DIR_PATH'
+   )
 
 def register():
-    bpy.utils.register_class(ForciTextureReplaceOperator)
-    bpy.utils.register_class(ForciTextureReplacePanel)
-    bpy.utils.register_class(ForciTexturePreferences)
+   bpy.utils.register_class(ForciTextureReplaceOperator)
+   bpy.utils.register_class(ForciTextureReplacePanel)
+   bpy.utils.register_class(ForciTexturePreferences)
 
 def unregister():
-    bpy.utils.unregister_class(ForciTextureReplaceOperator)
-    bpy.utils.unregister_class(ForciTextureReplacePanel)
-    bpy.utils.unregister_class(ForciTexturePreferences)
+   bpy.utils.unregister_class(ForciTextureReplaceOperator)
+   bpy.utils.unregister_class(ForciTextureReplacePanel)
+   bpy.utils.unregister_class(ForciTexturePreferences)
 
 if __name__ == "__main__":
-    register()
+   register()
